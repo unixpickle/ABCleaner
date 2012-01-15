91c40efbeafb59e9a8f9cae1283068df25da6e62
Alex Nichol <alexnichol@comcast.net>
2012-01-15 15:05:27 -0500

    Initial Commit

---
+++ ABCleaner/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ABCleaner
+//
+//  Created by Alex Nichol on 1/15/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ ABCleaner/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  ABCleaner
+//
+//  Created by Alex Nichol on 1/15/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
